e14ccdb61bb6a312681367e1f01c1480efa9cf3b
ThanhMai6507 <main6507@gmail.com>
2023-03-17 16:40:41 +0700

    make seeder for all tables

---
+++ database/factories/ProductSizeFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Product;
+use App\Models\Size;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\ProductSize>
+ */
+class ProductSizeFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'product_id' => fake()->randomElement(Product::pluck('id')),
+            'size_id' => fake()->randomElement(Size::pluck('id')),
+        ];
+    }
+}
