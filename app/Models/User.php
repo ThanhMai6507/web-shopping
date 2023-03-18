2f8a084474782ad62319a0832c4cfe957f8951c9
ThanhMai6507 <main6507@gmail.com>
2023-03-18 16:32:49 +0700

    send email when register

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,13 +4,14 @@ declare(strict_types=1);
 
 namespace App\Models;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
+use App\Notifications\VerifyEmail;
 
-class User extends Authenticatable
+class User extends Authenticatable implements MustVerifyEmail
 {
     use HasApiTokens;
     use HasFactory;
@@ -49,5 +50,10 @@ class User extends Authenticatable
     public function products()
     {
         return $this->belongsToMany(Product::class);
+    }
+
+    public function sendEmailVerificationNotification()
+    {
+        $this->notify((new VerifyEmail())->onQueue('default'));
     }
 }
