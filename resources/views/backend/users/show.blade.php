53916e75a796ba9b4391c0a302fc257de10cd92d
ThanhMai6507 <main6507@gmail.com>
2023-03-24 15:17:42 +0700

    get information of user

--- resources/views/backend/users/show.blade.php
+++ resources/views/backend/users/show.blade.php
@@ -2,7 +2,27 @@
 
 @section('content')
     <h1>Detail User</h1>
-
+    <br>
+    
+    <div class="row settings-tab">
+        <div class="col-md-4">
+            <div class="card">
+                <div class="card-header">
+                    <h4 class="card-title">Thông tin User</h4>
+                </div>
+                <div class="card-body" style="display:flex; gap:30px">
+                    <div class="form-group">
+                        <label>Name</label>
+                        <p>{{ $user->name }}</p>
+                    </div>
+                    <div class="form-group">
+                        <label>Email</label>
+                        <p>{{ $user->email }}</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <br>
     <div class="col-md-8">
         <div class="card">
             <div class="card-body p-0">
@@ -23,6 +43,10 @@
                             <td>{{ $product->description }}</td>
                             <td>{{ $product->category->name }}</td>
                         </tr>
+
+                        <a href="{{ route('users.edit', ['user' => $user->id]) }}">Update</a>
+                        <a href="{{ route('users.show', ['user' => $user->id]) }}">Infomation</a>
+                        
                         @endforeach
                     </tbody>
                 </table>
