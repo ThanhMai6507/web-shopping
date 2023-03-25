af277e53da4bd4830517f7985b3b072bafd46f60
ThanhMai6507 <main6507@gmail.com>
2023-03-25 22:13:35 +0700

    edit button delete

--- resources/views/components/button_delete.blade.php
+++ resources/views/components/button_delete.blade.php
@@ -1,5 +1,8 @@
 <form class="delete" action="{{ $route }}" method="post">
     @csrf
     @method('DELETE')
-    <input class="btn btn-primary" type="submit" name="button_delete" value="Delete" onclick="return confirm('Are you sure to delete this item?')">
+    <a href="{{ route('users.show', ['user' => $user->id]) }}" style="background: #F0D5DA;"
+        class="btn btn-sm bg-success-light me-2" name="button_delete" value="Delete" onclick="return confirm('Are you sure to delete this item?')">
+        <i data-feather="trash-2" width="16" color="#E63C3C"></i>
+    </a>
 </form>
