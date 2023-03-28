e9cb88a47df8f46c57173d167e734064fc5d8944
ThanhMai6507 <main6507@gmail.com>
2023-03-28 17:20:00 +0700

    set template for user and admin

--- app/Http/Controllers/admin/DashboardController.php
+++ app/Http/Controllers/admin/DashboardController.php
@@ -5,11 +5,16 @@ declare(strict_types=1);
 namespace App\Http\Controllers\admin;
 
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Http\Request;
 
 class DashboardController extends Controller
 {
-    public function index()
+    public function index(Request $request)
     {
-        return view('backend.dashboard.index');
+        if (Auth::check() && Auth::user()->type == 1) {
+            return view('backend.dashboard.index');
+        }
+        return view('user.dashboard.index');
     }
 }
